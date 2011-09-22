c0d8a11e009b0b0dedf72b9d2191b6967a973bd4
Chuck <cthier@gmail.com>
2011-09-22 16:23:56 -0500

    Adding fuse python version

--- fusetarget.py
+++ fusetarget.py
@@ -1,11 +1,13 @@
-import errno
+#!/usr/bin/env python
+
+"""uses fuse python bindings"""
 import os
 import math
 import sys
 import time
+import errno
 
-import llfuse
-import xattr
+import fuse
 
 from directio import DirectFile
 
@@ -14,15 +16,16 @@ INODE_DIR = 1
 INODE_VOLUME = 2
 BLOCK_SIZE = 1024*4
 
-class Operations(llfuse.Operations):
-    def __init__(self, path):
-        super(Operations, self).__init__()
+
+class TargetFuse(fuse.Fuse):
+    def __init__(self, path, *args, **kw):
+        fuse.Fuse.__init__(self, *args, **kw)
         self.path = path
         self.vol = DirectFile(path)
 
     def _get_vol_attrs(self):
         stat = os.lstat(self.path)
-        attrs = llfuse.EntryAttributes()
+        attrs = fuse.Stat()
         attrs.st_ino = INODE_VOLUME
         attrs.generation = 0
         attrs.entry_timeout = 300
@@ -42,7 +45,7 @@ class Operations(llfuse.Operations):
 
     def _get_dir_attrs(self):
         stat = os.lstat('/'.join(self.path.split('/')[:-1]))
-        attrs = llfuse.EntryAttributes()
+        attrs = fuse.Stat()
         attrs.st_ino = INODE_DIR
         attrs.generation = 0
         attrs.entry_timeout = 300
@@ -60,141 +63,109 @@ class Operations(llfuse.Operations):
         attrs.st_ctime = stat.st_ctime
         return attrs
 
-    def access(self, inode, mode, ctx):
-        """Check access"""
-        print "ACCESS inode=%s mode=%s" % (inode, mode)
-        return True
-
-    def create(self, inode_p, name, mode, ctx):
-        print "!CREATE! inode_p=%s name=%s mode=%s" % (inode_p, name, mode)
-        return super(Operations, self).create(inode_p, name, mode, ctx)
-
-    def destroy(self):
-        """Clean up"""
-        print "DESTROY"
-        self.vol = None
-
-    def flush(self, fh):
-        print "!FLUSH! fh=%s" % fh
-        return super(Operations, self).flush(fh)
-
-    def forget(self, inode, nlookup):
-        print "!FORGET! inode=%s nlookup=%s" % (inode, nlookup)
-        return super(Operations, self).forget(inode, nlookup)
-
-    def fsync(self, fh, datasync):
-        print "!FSYNC! fh=%s datasync=%s" % (fs, datasync)
-        return super(Operations, self).fsync(inode, nlookup)
-
-    def fsyncdir(self, fh, datasync):
-        print "!FSYNC! fh=%s datasync=%s" % (fs, datasync)
-        return super(Operations, self).fsyncdir(inode, nlookup)
-
-    def getattr(self, inode):
-        print "GETATTR inode=%s" % inode
-        if inode == INODE_VOLUME:
-            return self._get_vol_attrs()
-        if inode == INODE_DIR:
+    def getattr(self, path):
+        print "GETATTR path=%s" % path
+        if path == '/':
             return self._get_dir_attrs()
-        raise llfuse.FUSEError(errno.ENOENT)
+        elif path == '/volume':
+            return self._get_vol_attrs()
+        else:
+            return errno.ENOENT
 
-    def getxattr(self, inode, name):
-        print "GETXATTR inode=%s name=%s" % (inode, name)
-        if inode != INODE_VOLUME:
-            raise llfuse.FUSEError(errno.ENOENT)
-        raise llfuse.FUSEError(llfuse.ENOATTR)
 
-    def listxattr(self, inode):
-        print "!LISTXATTR! inode=%s" % inode
-        return super(Operations, self).fsyncdir(inode, nlookup)
+    def readdir(self, path, offset):
+        print '*** readdir', path, offset
+        dirents = ['.', '..', 'volume']
+        for r in dirents:
+            yield fuse.Direntry(r)
 
-    def lookup(self, inode_p, name):
-        print "LOOKUP inode_p=%s name=%s" % (inode_p, name)
-        if name == '.' or name == '..':
-            return inode_p
-        if name != 'volume':
-            raise llfuse.FUSEError(errno.ENOENT)
-        return self._get_vol_attrs()
+    def mythread ( self ):
+        print '*** mythread'
+        return -errno.ENOSYS
 
-    def open(self, inode, flags):
-        print "OPEN inode=%s flags=%s" % (inode, flags)
-        if inode != INODE_VOLUME:
-            raise llfuse.FUSEError(errno.ENOENT)
-        return INODE_VOLUME
+    def chmod ( self, path, mode ):
+        print '*** chmod', path, oct(mode)
+        return -errno.ENOSYS
 
-    def opendir(self, inode):
-        print "OPENDIR inode=%s" % inode
-        return INODE_DIR
+    def chown ( self, path, uid, gid ):
+        print '*** chown', path, uid, gid
+        return -errno.ENOSYS
 
-    def read(self, fh, off, size):
-        print "READ fh=%s, off=%s, size=%sK" % (fh, off, size/1024)
-        if fh != INODE_VOLUME:
-            raise llfuse.FUSEError(errno.ENOENT)
-        return self.vol.pread(off, size)
+    def fsync ( self, path, isFsyncFile ):
+        print '*** fsync', path, isFsyncFile
+        return 0
+        return -errno.ENOSYS
 
-    def readdir(self, fh, off):
-        print "READDIR: fh=%s off=%s" % (fh, off)
-        if fh != INODE_DIR:
-            raise llfuse.FUSEError(errno.ENOENT)
-        if off == 0:
-            yield ('volume', self._get_vol_attrs(), 1)
+    def link ( self, targetPath, linkPath ):
+        print '*** link', targetPath, linkPath
+        return -errno.ENOSYS
 
-    def release(self, fh):
-        print "RELEASE: fh=%s" % fh
-        pass
+    def mkdir ( self, path, mode ):
+        print '*** mkdir', path, oct(mode)
+        return -errno.ENOSYS
 
-    def releasedir(self, fh):
-        print "RELEASE DIR: fh=%s" % fh
-        pass
+    def mknod ( self, path, mode, dev ):
+        print '*** mknod', path, oct(mode), dev
+        return -errno.ENOSYS
 
-    def removexattr(self, inode, name):
-        print "!REMOVEXATTR! inode=%s attr=%s" % (inode, name)
-        return super(Operations, self).removexattr(inode, name)
+    def open ( self, path, flags ):
+        print '*** open', path, flags
+        return 0
+        return -errno.ENOSYS
 
-    def setattr(self, inode, attr):
-        print "!SETATTR! inode=%s name=%s" % (inode, attr)
-        return super(Operations, self).setattr(indode, attr)
+    def read ( self, path, length, offset ):
+        print '*** read', path, length, offset
+        if path == '/volume':
+            return self.vol.pread(offset, length)
+        return errno.ENOENT
 
-    def setxattr(self, inode, name, value):
-        print "!SETXATTR! inode=%s name=%s value=%s" % (inode, name, value)
-        return super(Operations, self).setxattr(inode, name, value)
+    def readlink ( self, path ):
+        print '*** readlink', path
+        return -errno.ENOSYS
 
-    def statfs(self):
-        print "STATFS"
-        stat = os.lstat(self.path)
-        attrs = llfuse.StatvfsData()
-        attrs.f_bsize = BLOCK_SIZE
-        attrs.f_frsize = stat.st_size
-        attrs.f_blocks = math.ceil(stat.st_size/float(BLOCK_SIZE))
-        attrs.f_bfree = 0
-        attrs.f_bavail = 0
-        attrs.f_files = 1
-        attrs.f_ffree = 0
-        attrs.f_favail = 0
-        return attrs
+    def release ( self, path, flags ):
+        print '*** release', path, flags
+        return 0
 
-    def write(self, fh, off, buf):
-        print "WRITE fh=%s off=%s len=%sK" % (fh, off, len(buf)/1024)
-        if fh != INODE_VOLUME:
-            raise llfuse.FUSEError(errno.ENOENT)
-        amt = self.vol.pwrite(off, buf)
-        return amt
+    def rename ( self, oldPath, newPath ):
+        print '*** rename', oldPath, newPath
+        return -errno.ENOSYS
+
+    def rmdir ( self, path ):
+        print '*** rmdir', path
+        return -errno.ENOSYS
+
+    def statfs ( self ):
+        print '*** statfs'
+        return -errno.ENOSYS
+
+    def symlink ( self, targetPath, linkPath ):
+        print '*** symlink', targetPath, linkPath
+        return -errno.ENOSYS
+
+    def truncate ( self, path, size ):
+        print '*** truncate', path, size
+        return -errno.ENOSYS
+
+    def unlink ( self, path ):
+        print '*** unlink', path
+        return -errno.ENOSYS
+
+    def utime ( self, path, times ):
+        print '*** utime', path, times
+        return -errno.ENOSYS
+
+    def write ( self, path, buf, offset ):
+        #print '*** write', path, offset
+        if path == '/volume':
+            return self.vol.pwrite(offset, buf)
+        return errno.ENOENT
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print ('Usage: %s PATH MOUNTPOINT' % sys.argv[0])
-        sys.exit(1)
-    path = sys.argv[1]
-    mountpoint = sys.argv[2]
-    operations = Operations(path)
-    llfuse.init(operations, mountpoint,
-            [ b'fsname=lunr'])
-
-    try:
-        llfuse.main(single=True)
-    except Exception as e:
-        print e
-    finally:
-        llfuse.close(unmount=False)
-        time.sleep(10)
-    llfuse.close()
+    fuse.fuse_python_api = (0, 2)
+    path = '/srv/vol1'
+    fs = TargetFuse(path, version="% prog ")
+    #fs.flags = 0
+    fs.multithread = 0
+    fs.parse(errex=1)
+    fs.main()
